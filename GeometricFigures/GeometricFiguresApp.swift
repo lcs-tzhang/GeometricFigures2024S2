83da3a6dbeffc63d44fdaadfadb9a4a443e59ba8
Tiffany Zhang <tzhang@lcs.on.ca>
2024-10-28 09:42:40 -0400

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by xinyu zhang on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
